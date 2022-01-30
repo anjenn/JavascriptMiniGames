774e20bad3139c73696c3e2637c8984278c493d7
anjenn <93240896+anjenn@users.noreply.github.com>
2022-01-31 00:20:15 +0900

    show and hide classes

--- Modal Windows/script.js
+++ Modal Windows/script.js
@@ -3,8 +3,22 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 //we can treat the above as an array
-console.log(btnOpenModal);
+console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModa.length; i++) {}
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+/*we shouldn't write closeModal(), because it's gonna call the function immediately
+NOT as we click.*/
+overlay.addEventListener('click', closeModal);
