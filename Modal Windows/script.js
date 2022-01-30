6c50ad67c7584c68da227b05076b659a4d6eb0dd
anjenn <93240896+anjenn@users.noreply.github.com>
2022-01-30 23:55:38 +0900

    query select all & array

--- Modal Windows/script.js
+++ Modal Windows/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+//we can treat the above as an array
+console.log(btnOpenModal);
+
+for (let i = 0; i < btnsOpenModa.length; i++) {}
